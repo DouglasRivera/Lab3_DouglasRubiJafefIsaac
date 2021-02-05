2bf3b20671df282c502ab8d5813a90472f8dd219
isaacjafetm <78220414+isaacjafetm@users.noreply.github.com>
2021-02-05 14:37:12 -0600

    wenas

--- Lab3_DouglasyJafef/src/lab3_douglasyjafef/Lab3_DouglasyJafef.java
+++ Lab3_DouglasyJafef/src/lab3_douglasyjafef/Lab3_DouglasyJafef.java
@@ -5,6 +5,8 @@
  */
 package lab3_douglasyjafef;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author famii
@@ -15,7 +17,120 @@ public class Lab3_DouglasyJafef {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int opcion=0;
+        while(opcion!=7){
+            opcion=Integer.parseInt(JOptionPane.showInputDialog("1. Corporaciones\n" +
+                    "2. Marcas\n" +
+                    "3. Modelos\n" +
+                    "4. Fábricas\n" +
+                    "5. Divisiones tecnológicas.\n" +
+                    "6. Informe total.\n"
+                    + "7. Salir"));
+            switch(opcion){
+                case 1:{
+                    int op = Integer.parseInt(JOptionPane.showInputDialog("Que desea hacer?\n 1. Crear\n 2. Listar\n 3.Modificar\n 4. Eliminar"));
+                    switch(op){
+                        case 1:{
+                            
+                            break;}
+                        case 2:{
+                            
+                            break;}
+                        case 3:{
+                            
+                            break;}
+                        case 4:{
+                            
+                            break;}
+                    }
+                    break;
+                }
+                case 2:{
+                    int op = Integer.parseInt(JOptionPane.showInputDialog("Que desea hacer?\n 1. Crear\n 2. Listar\n 3.Modificar\n 4. Eliminar"));
+                    switch(op){
+                        case 1:{
+                            
+                            break;}
+                        case 2:{
+                            
+                            break;}
+                        case 3:{
+                            
+                            break;}
+                        case 4:{
+                            
+                            break;}
+                    }
+                    break;
+                }
+                case 3:{
+                    int op = Integer.parseInt(JOptionPane.showInputDialog("Que desea hacer?\n 1. Crear\n 2. Listar\n 3.Modificar\n 4. Eliminar"));
+                    switch(op){
+                        case 1:{
+                            
+                            break;}
+                        case 2:{
+                            
+                            break;}
+                        case 3:{
+                            
+                            break;}
+                        case 4:{
+                            
+                            break;}
+                    }
+                    break;
+                }
+                case 4:{
+                    int op = Integer.parseInt(JOptionPane.showInputDialog("Que desea hacer?\n 1. Crear\n 2. Listar\n 3.Modificar\n 4. Eliminar\n 5. Agregar modelo"));
+                    switch(op){
+                        case 1:{
+                            
+                            break;}
+                        case 2:{
+                            
+                            break;}
+                        case 3:{
+                            
+                            break;}
+                        case 4:{
+                            
+                            break;}
+                        case 5:{
+                            
+                            break;}
+                    }
+                    break;
+                }
+                case 5:{
+                    int op = Integer.parseInt(JOptionPane.showInputDialog("Que desea hacer?\n 1. Crear\n 2. Listar\n 3.Modificar\n 4. Eliminar\n 5. Asignar vehiculo"));
+                    switch(op){
+                        case 1:{
+                            
+                            break;}
+                        case 2:{
+                            
+                            break;}
+                        case 3:{
+                            
+                            break;}
+                        case 4:{
+                            
+                            break;}
+                        case 5:{
+                            
+                            break;}
+                    }
+                    break;
+                }
+                case 6:{
+                    JOptionPane.showMessageDialog(null, "");
+                    break;
+                }
+            }   
+        }
+        
+        
     }
     
 }
