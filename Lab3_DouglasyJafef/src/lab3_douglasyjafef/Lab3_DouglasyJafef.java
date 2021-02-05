a36c22e2ef2c3c3a77e5ae7244f8c9d8cab3c837
isaacjafetm <78220414+isaacjafetm@users.noreply.github.com>
2021-02-05 16:42:57 -0600

    pinche marca

--- Lab3_DouglasyJafef/src/lab3_douglasyjafef/Lab3_DouglasyJafef.java
+++ Lab3_DouglasyJafef/src/lab3_douglasyjafef/Lab3_DouglasyJafef.java
@@ -20,6 +20,7 @@ public class Lab3_DouglasyJafef {
     public static void main(String[] args) {
         int opcion=0;
         ArrayList lista = new ArrayList();
+        Corporaciones c = new Corporaciones();
         while(opcion!=7){
             opcion=Integer.parseInt(JOptionPane.showInputDialog("1. Corporaciones\n" +
                     "2. Marcas\n" +
@@ -34,26 +35,57 @@ public class Lab3_DouglasyJafef {
                     switch(op){
                         case 1:{
                             String nombre = JOptionPane.showInputDialog("Ingrese nombre");
-                            String Sede = JOptionPane.showInputDialog("Ingrese nombre");
-                            int año = Integer.parseInt(JOptionPane.showInputDialog("Ingrese nombre"));
-                            String presi = JOptionPane.showInputDialog("Ingrese nombre");
-                            int empleados = Integer.parseInt(JOptionPane.showInputDialog("Ingrese nombre"));
-                            int autos = Integer.parseInt(JOptionPane.showInputDialog("Ingrese nombre"));
+                            String Sede = JOptionPane.showInputDialog("Ingrese sede");
+                            int año = Integer.parseInt(JOptionPane.showInputDialog("Ingrese año de fundación"));
+                            String presi = JOptionPane.showInputDialog("Ingrese nombre del Presidente");
+                            int empleados = Integer.parseInt(JOptionPane.showInputDialog("Ingrese número de empleados"));
+                            int autos = Integer.parseInt(JOptionPane.showInputDialog("Ingrese número de autos"));
                             lista.add(new Corporaciones(nombre, Sede, año, presi, empleados, autos));
                             break;}
                         case 2:{
-                            JOptionPane.showMessageDialog(null, lista);
+                            String salida ="";
+                            for (Object t : lista) {
+                                if (t instanceof Corporaciones) {
+                                    salida += lista.indexOf(t)+"- "+t+"\n";
+                                }
+                            }
+                            JOptionPane.showMessageDialog(null, salida);
                             break;}
                         case 3:{
                             int pos = Integer.parseInt(JOptionPane.showInputDialog("Ingrsese posición a modificar"));
                             if(lista.get(pos) instanceof Corporaciones){
-                                int mod =Integer.parseInt(JOptionPane.showInputDialog("Ingrsese posición a modificar"));
+                                int mod =Integer.parseInt(JOptionPane.showInputDialog("Que desea modificar?\n 1. Nombre\n 2. Sede\n 3. Año de fundación\n 4. Nombre de Presidente \n 5. Cantidad de empleados\n 6. Número de autos"));
+                                switch(mod){
+                                    case 1:{
+                                        String nombre=JOptionPane.showInputDialog("Ingrsese nuevo nombre");
+                                        break;}
+                                    case 2:{
+                                        String sede=JOptionPane.showInputDialog("Ingrsese nueva sede");
+                                        break;}
+                                    case 3:{
+                                        int año=Integer.parseInt(JOptionPane.showInputDialog("Ingrsese nuevo año de fundación"));
+                                        break;}
+                                    case 4:{
+                                        String presi=JOptionPane.showInputDialog("Ingrsese nuevo nombre de presidente");
+                                        break;}
+                                    case 5:{
+                                        int empleados=Integer.parseInt(JOptionPane.showInputDialog("Ingrsese nueva cantidad de empleados"));
+                                        break;}
+                                    case 6:{
+                                        int autos=Integer.parseInt(JOptionPane.showInputDialog("Ingrsese nuevo número de autos"));
+                                        break;}
+                                }
                             }else {
                                 JOptionPane.showMessageDialog(null, "Posición no válida");
                             }
                             break;}
                         case 4:{
-                            
+                            int pos = Integer.parseInt(JOptionPane.showInputDialog("Ingrsese posición a modificar"));
+                            if (lista.get(pos) instanceof Corporaciones){
+                                lista.remove(pos);
+                            }else{
+                                JOptionPane.showMessageDialog(null, "Posición no válida");
+                            }
                             break;}
                     }
                     break;
@@ -62,10 +94,36 @@ public class Lab3_DouglasyJafef {
                     int op = Integer.parseInt(JOptionPane.showInputDialog("Que desea hacer?\n 1. Crear\n 2. Listar\n 3.Modificar\n 4. Eliminar"));
                     switch(op){
                         case 1:{
-                            
+                            String salida ="";
+                            for (Object t : lista) {
+                                if (t instanceof Corporaciones) {
+                                    salida += lista.indexOf(t)+"- "+t+"\n";
+                                }
+                            }
+                            int pos = Integer.parseInt(JOptionPane.showInputDialog(salida+"Ingrese que la posición de la corperación a la cual le desea añadir una marca"));
+                            if(lista.get(pos) instanceof Corporaciones){
+                                String Nombre=JOptionPane.showInputDialog("Ingrese nombre de la marca");
+                                String Slogan=JOptionPane.showInputDialog("Ingrese slogan de la marca");
+                                int yearFundacion=Integer.parseInt(JOptionPane.showInputDialog("Ingrese año de fundación de la marca"));
+                                int yearIntegracion=Integer.parseInt(JOptionPane.showInputDialog("Ingrese año de integración a la copreoración de la marca"));
+                                String Fundador=JOptionPane.showInputDialog("Ingrese nombre del fundador");
+                                int TotalVentas=Integer.parseInt(JOptionPane.showInputDialog("Ingrese total de ventas de la marca"));
+                                String Presidente=JOptionPane.showInputDialog("Ingrese nombre del presidente de la marca");
+                                int NumeroModelos=Integer.parseInt(JOptionPane.showInputDialog("Ingrese número de modelos que tiene la marca"));
+                                new Marca(Nombre, Slogan, yearFundacion, yearIntegracion, Fundador, TotalVentas, Presidente, NumeroModelos);
+                                ((Corporaciones).lista.get(pos)).getMarca().add(new Marca(Nombre, Slogan, yearFundacion, yearIntegracion, Fundador, TotalVentas, Presidente, NumeroModelos));
+                            }else{
+                                JOptionPane.showMessageDialog(null,"No hay Corporación en esa posición");
+                            }
                             break;}
                         case 2:{
-                            
+                            String salida ="";
+                            for (Object t : lista) {
+                                if (t instanceof Marca) {
+                                    salida += lista.indexOf(t)+"- "+t+"\n";
+                                }
+                            }
+                            JOptionPane.showMessageDialog(null, salida);
                             break;}
                         case 3:{
                             
